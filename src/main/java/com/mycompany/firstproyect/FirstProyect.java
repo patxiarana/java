7de9788fceda95ffb6d359c670ba5fe061e503df
patxi <patxiarana05@gmail.com>
2023-05-09 11:25:03 -0300

    first-java

---
+++ src/main/java/com/mycompany/firstproyect/FirstProyect.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstproyect;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FirstProyect {
+
+    public static void main(String[] args) {
+        //// datos primitivos ////
+      
+//int entero = 12456568  ;
+        //float decimal = 3.45f ;
+       //double decimal = 3.4523421 ;
+      //   char caracter = 'a' ;
+      // boolean decision = false ;
+      //// No primitivos ///
+      
+      // Integer numero = 10 ;
+    //  Integer numero = null ;
+      
+    // Cadenas /// 
+    
+  //  String palabra = "Hola Mundo" ;
+    
+    
+      
+    //    System.out.println("numero decimal:" +  palabra);
+    
+  //  entero = 12 ;
+    }
+}
